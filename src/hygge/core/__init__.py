ea1e20e6de465693034ca8d2ac2c3b15110b8abe
Nick Teff <nicholas.teff@gmail.com>
2025-11-14 22:34:56 -0600

    create workspace

--- src/hygge/core/__init__.py
+++ src/hygge/core/__init__.py
@@ -5,14 +5,16 @@ from .coordinator import Coordinator, CoordinatorConfig
 from .flow import Flow, FlowConfig
 from .home import Home, HomeConfig
 from .store import Store, StoreConfig
+from .workspace import Workspace
 
 __all__ = [
     "Coordinator",
+    "CoordinatorConfig",
     "Flow",
     "FlowConfig",
     "Home",
     "HomeConfig",
     "Store",
     "StoreConfig",
-    "CoordinatorConfig",
+    "Workspace",
 ]
